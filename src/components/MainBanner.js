07f74352e6684a14325bb92f8d366583b644c68f
getaudio2 <paugermaisar@hotmail.es>
2024-04-28 23:28:18 +0200

    Added h1 tag for intro message after welcome message and lets connect button

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -8,6 +8,9 @@ export const MainBanner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my portfolio!</span>
+                        <h1>{`Hi I'm Paul`}<span className="wrap">web developer</span></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <button onClick={() => console.log('connect')}>Let's connect</button>
                     </Col>
                 </Row>
             </Container>
