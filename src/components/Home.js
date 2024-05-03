125e0f9c4086e5d9d20e15a5cf4bcbc376856aea
getaudio2 <paugermaisar@hotmail.es>
2024-05-03 22:41:32 +0200

    Download CV and contact buttons added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Home.css'
+import CV from '../assets/CV Paúl Maigua S_v03.pdf'
 
 const Home = () => {
   return (
     <section className="banner" id="home">
         <div className="container header_container">
-            <h5>Hello I'm Paúl Maigua, Software Developer</h5>
+            <h5>Hello I'm Paúl Maigua</h5>
+            <h5 className="text-light">Software Developer</h5>
+        </div>
+        <div className="cv">
+            <a href={CV} download>Download CV</a>
+            <a href="#contact">Let's Connect</a>
         </div>
     </section>
   )
