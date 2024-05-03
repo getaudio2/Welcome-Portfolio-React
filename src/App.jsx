1e5859577ba83b2849ec7f622f4cf53a1165b11f
getaudio2 <paugermaisar@hotmail.es>
2024-05-03 22:34:22 +0200

    Added Home script/css. Added general CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <Home />
+    </>
   )
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <section className="banner" id="home">
+        <div className="container header_container">
+            <h5>Hello I'm Paúl Maigua, Software Developer</h5>
+        </div>
+    </section>
+  )
+}
+
+export default Home
