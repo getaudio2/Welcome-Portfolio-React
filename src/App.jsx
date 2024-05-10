48eeea930f8480547efcff841c4e39ddbd7ec271
getaudio2 <paugermaisar@hotmail.es>
2024-05-10 23:17:48 +0200

    Created files for each section of the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import Profile from './components/Profile'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Home />
+      <Profile />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,10 +11,12 @@ const Home = () => {
             <h5>Hello I'm Paúl Maigua</h5>
             <h5 className="text-light">Software Developer</h5>
         </div>
+
         <div className="cv">
             <a href={CV} download className="btn">Download CV</a>
             <a href="#contact" className="btn btn-primary">Let's Connect</a>
         </div>
+        
         <div className="header__socials">
           <a href="https://www.linkedin.com/in/paúl-gerard-maigua-sarango-342896206/" target="_blank"><FaLinkedin /></a>
           <a href="https://github.com/getaudio2" target="_blank"><FaGithub /></a>
