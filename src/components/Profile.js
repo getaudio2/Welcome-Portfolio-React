1a8da67b2c64554f3556bc7f256d5f26efb79a07
getaudio2 <paugermaisar@hotmail.es>
2024-05-20 18:11:56 +0200

    Profile section with image portrait and about text

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Profile.css'
+import ME from '../assets/miFoto.jpg'
 
 const Profile = () => {
   return (
-    <section id='profile'>Profile</section>
+    <section id='profile'>
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+
+      <div className='container profile__container'>
+        <div className='profile-image'>
+          <img src={ME} alt="About Me" />
+        </div>
+        <div className='profile__content'>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
