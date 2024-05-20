2ac28e2bd6dc8767933f4a10c193ddb8b37ceb6e
getaudio2 <paugermaisar@hotmail.es>
2024-05-20 17:00:27 +0200

    Navbar updates active section on click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,53 +1,16 @@
-/*import { useState, useEffect } from "react";
-import {Navbar, Nav, Container} from "react-bootstrap";*/
-import './NavBar.css'
+import { useState } from "react";
+import './NavBar.css';
 
 const NavBar = () => {
-    /*const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+    const [activeNav, setActiveNav] = useState('#');
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) setScrolled(true);
-            else setScrolled(false);
-        }
-
-        window.addEventListener("scroll", onScroll); // Sets scrolled to TRUE if user scrolls 50px down the window page
-
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
-
-    const onUpdateActiveLink = (value) => { // Updates the section that the user is currently in
-        setActiveLink(value);
-    }*/
-
-    return ( // Upper navigation bar section with buttons to move between the portfolio sections
-        /*<Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-            <Container>
-                <Navbar.Brand href="#home">Logo</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
-                    <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
-                    <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects`")}>Projects</Nav.Link>
-                </Nav>
-                <span className="navbar-text">
-                    Texto de prueba
-                </span>
-                <br/>
-                <button className="vvd" onClick={() => console.log('connect')}>
-                    <span>Let's connect</span>
-                </button>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>*/
+    return ( // Navigation bar section with buttons to move between the portfolio sections
         <nav>
-            <a href="#" className='active'>Home</a>
-            <a href="#profile">Profile</a>
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
+            <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>Home</a>
+            <a href="#profile" onClick={() => setActiveNav('#profile')} className={activeNav === '#profile' ? 'active' : ''}>Profile</a>
+            <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}>Skills</a>
+            <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}>Projects</a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>Contact</a>
         </nav>
     )
 }
