baaf5bddd7b14fd511db26603e012511724f3d73
getaudio2 <paugermaisar@hotmail.es>
2024-06-06 18:41:02 +0200

    Fixed emailjs error 400, added responsive css for small devices

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,48 @@ import React from 'react'
 import './Contact.css'
 import { MdOutlineAttachEmail } from "react-icons/md";
 import { FaWhatsapp } from "react-icons/fa";
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the form from submitting
+
+    // Define the regular expression for email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+
+    if (email.match(emailRegex)) { // email.match() to validate the email
+      // Valid email address
+      sendEmail(e);
+      setEmailError("Email sent successfully!");
+    } else if (email === "") {
+      setEmailError("Email is empty!");
+    } else {
+      setEmailError("Invalid email address!");
+    }
+  };
+
   const form = useRef();
 
   // EmailJS function
   const sendEmail = (e) => {
     e.preventDefault();
-
+    emailjs.init('_hUxKPwuzPeD9yWBx');
     emailjs
-      .sendForm('service_oxgzamb', 'template_ujab0gc', form.current, {
-        publicKey: 'A7qD41T7UvKy_1-jM',
-      })
+      .sendForm('service_oxgzamb', 'template_ujab0gc', form.current).then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+
+    e.target.reset();
+    setEmail('');
   };
 
   return (
@@ -37,10 +65,26 @@ const Contact = () => {
             <a href='https://api.whatsapp.com/send?phone=34662239738' target='_blank'>Send a message</a>
           </article>
         </div>
-        <form ref={form} onSubmit={sendEmail}>
-          <input type='text' name='name' placeholder='Your Full Name' required />
-          <input type='email' name='email' placeholder='Your Email' required />
-          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+        <form ref={form} onSubmit={handleSubmit}>
+          <input 
+            type='text' 
+            name='name' 
+            placeholder='Your Full Name' required 
+          />
+          <input 
+            type='email' 
+            name='email' 
+            placeholder='Your Email' required 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea 
+            name='message' 
+            rows='7' 
+            placeholder='Your Message' required
+          ></textarea>
+
+          {emailError && <div style={{ color: 'white' }}>{emailError}</div>}
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './NavBar.css';
 
 const NavBar = () => {
     const [activeNav, setActiveNav] = useState('#');
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const sections = document.querySelectorAll('section');
+          const header = document.querySelector('header');
+          const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+          const windowHeight = window.innerHeight;
+          const documentHeight = document.documentElement.scrollHeight;
+    
+          for (let i = sections.length - 1; i >= 0; i--) {
+            const section = sections[i];
+            const sectionId = `#${section.id}`;
+            const sectionOffsetTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+    
+            if (
+              scrollPosition >= sectionOffsetTop - windowHeight * 0.7 &&
+              (scrollPosition < sectionOffsetTop + sectionHeight ||
+                scrollPosition >= documentHeight - windowHeight)
+            ) {
+              setActiveNav(sectionId);
+              break;
+            }
+          }
+    
+          if (
+            scrollPosition < header.offsetHeight - 300
+          ) {
+            setActiveNav('#');
+          }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+    
 
     return ( // Navigation bar section with buttons to move between the portfolio sections
         <nav>
